cc78c4ad763932373cb9c1a12b00d783ff944500
Faustino Santos <faus.santos@hotmail.com>
2023-08-23 20:25:56 -0300

    Actividad almacenar

---
+++ Actividad almacenar.js
@@ -0,0 +1,34 @@
+    let espacio = document.getElementById("contenedor")
+    let palabras = document.getElementsByTagName("li")
+    let ingresado
+
+
+document.getElementById("agregar").addEventListener("click", () =>{
+
+    let texto = document.getElementById("item").value
+
+    if(texto !== ""){
+        espacio.innerHTML += '<li>' + texto +'</li>'
+        ingresado = 'ingresado' + palabras.length;
+        localStorage.setItem(ingresado, texto)
+        document.getElementById("item").value = ""
+    }
+})
+
+document.getElementById("limpiar").addEventListener("click", () =>{
+    espacio.innerHTML = ""
+    localStorage.clear()
+})
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    let i = 1;
+    let ingresado;
+    ingresado = 'ingresado' + i;
+    while (localStorage.getItem(ingresado)) {
+        espacio.innerHTML += '<li>' + localStorage.getItem(ingresado) +'</li>';
+        i++
+        ingresado = 'ingresado' + i;
+    }
+
+})
